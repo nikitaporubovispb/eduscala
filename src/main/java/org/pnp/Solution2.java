ffecc450716ab08595f11edf9d6c25ef751df579
porubov <porubov@protei-lab.ru>
2024-03-03 17:35:27 +0300

    sol2. refactoring

--- src/main/java/org/pnp/Solution2.java
+++ src/main/java/org/pnp/Solution2.java
@@ -1,6 +1,8 @@
 package org.pnp;
 
 import java.util.Scanner;
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
 
 public class Solution2 {
     public static void main(String[] args) {
@@ -10,28 +12,45 @@ public class Solution2 {
         int result = 0;
 
         char[] chars = s.toCharArray();
-        aLoop: for (int a = 0; a < chars.length; a++) {
-            if (chars[a] == 'a') {
-                a++;
-                for (int b = a; b < chars.length; b++, a++) {
-                    if (chars[b] == 'b') {
-                        b++;
-                        for (int c = b; c < chars.length; c++, b++, a++) {
-                            if (chars[c] == 'c') {
-                                result++;
-                                continue aLoop;
-                            } else if (chars[c] == 'a') {
-                                continue aLoop;
-                            }
-                        }
-                    } else if (chars[b] == 'c') {
-                        continue aLoop;
-                    }
-                }
+        for (int i = 0; i < chars.length; i++) {
+            FindResult resultA = findA(chars, i);
+            if (resultA.find) {
+                result++;
+                i = resultA.i();
             }
         }
-
         System.out.println(result);
+    }
+
+    private record FindResult(boolean find, Integer i) {}
+
+    private static FindResult findA(char[] chars, int start) {
+        return find(chars, start, c -> c == 'a', c -> false, Solution2::findB);
+    }
+
+    private static FindResult findB(char[] chars, int a) {
+        return find(chars, ++a, c -> c == 'b', c -> c == 'a', Solution2::findC);
+    }
+
+    private static FindResult findC(char[] chars, int b) {
+        return find(chars, ++b, c -> c == 'c', c -> c == 'a', null);
+    }
+
+    private static FindResult find(char[] chars, int start,
+                                   Predicate<Character> isFind, Predicate<Character> isFall,
+                                   BiFunction<char[], Integer, FindResult> nextFind) {
+        for (int i = start; i < chars.length; i++) {
+            if (isFind.test(chars[i])) {
+                if (nextFind != null) {
+                    return nextFind.apply(chars, i);
+                } else {
+                    return new FindResult(true, i);
+                }
+            } else if (isFall.test(chars[i])) {
+                return new FindResult(false, null);
+            }
+        }
+        return new FindResult(false, null);
     }
 }
 
